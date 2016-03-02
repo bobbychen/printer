d7f0c15a754785aebd1bd11cfa3d02caecb89d3b
bobbychen <lovescaoliting@gmail.com>
2016-03-02 11:07:49 +0800

    service finish

--- src/com/thoughtworks/printer/service/PrintService.java
+++ src/com/thoughtworks/printer/service/PrintService.java
@@ -3,27 +3,22 @@ package com.thoughtworks.printer.service;
 import java.util.List;
 import java.util.Map;
 
+import com.thoughtworks.priter.Description;
+
 public interface PrintService {
 	
 	/**
-	 * 根据物品的条形码返回物品的销售方案
-	 * @param code
+	 * 根据传入的条形码及商品数量信息，进行计算
+	 * @param infos
 	 * @return
 	 */
-	public List<String> getSaleByCode(String code);
+	public List<Map<String,Description>> caculation(List<Map<String,Integer>> infos);
 	
 	/**
-	 *计算一种商品的价格 
-	 * @param code 物品条形码
+	 * 计算单品的价格
+	 * @param code
 	 * @param num
 	 * @return
 	 */
-	public float getUnitPriceByCode(String code,int num);
-
-	/**
-	 *返回计算总和
-	 * @param cart
-	 * @return
-	 */
-	public float getSumPay(Map<String,Integer> cart);
+	public Description caculation(String code,int num);
 }
